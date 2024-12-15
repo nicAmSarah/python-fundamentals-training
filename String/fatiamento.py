0f328f45c5ef1c22a232664e3df753c4820c52be
Sarah Nicacio <nicaciosarah085@gmail.com>
2024-12-15 09:45:05 -0300

    interpolation and slicing

---
+++ String/fatiamento.py
@@ -0,0 +1,8 @@
+nome = "sarah nicacio de amorim"
+
+print (nome[0])
+print (nome[-2])
+print (nome[:5])
+print (nome[10:])
+print (nome[10:16])
+print (nome[10:16:12])
---
+++ String/interpolation.py
@@ -0,0 +1,6 @@
+nome = "Sarah"
+idade = 18
+profissao = "aprendiz"
+linguagem = "Python"
+
+print("ola, me chamo {1}, tenho {2} anos e trabalho com {3}, utilizando {nome}" .format(nome=nome , idade=linguagem, profissao=profissao, idade=idade))
